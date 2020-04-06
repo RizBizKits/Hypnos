099e10427b90b1f1d808b88e98b0bbe45e66ae5d
Riz <rizakmalkhan@gmail.com>
2020-04-06 19:57:26 +0100

    Initial commit

---
+++ app/src/main/java/com/l/hypnos/MainActivity.java
@@ -0,0 +1,14 @@
+package com.l.hypnos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
