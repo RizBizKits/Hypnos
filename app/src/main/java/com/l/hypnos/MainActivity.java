97ec0110609a39a2e9197ff36b0732d228b8d446
Riz <rizakmalkhan@gmail.com>
2020-04-08 17:02:21 +0100

    add new alarm button

--- app/src/main/java/com/l/hypnos/MainActivity.java
+++ app/src/main/java/com/l/hypnos/MainActivity.java
@@ -1,14 +1,48 @@
 package com.l.hypnos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity  implements TimePickerDialog.OnTimeSetListener  {
+
+    private FloatingActionButton add_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add_btn = (FloatingActionButton) findViewById(R.id.add_btn);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+        });
+    }
+
+    public void showTimePickerDialog() {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Log.d("myHour", "hourOfDay: " + hourOfDay);
+        Log.d("myMin", "hourOfDay: " + minute);
     }
 }
+
